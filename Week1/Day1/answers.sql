fc2ee5eb653da170dfcfe7959a4b8c31a86448ef
tapaswipasupuleti <tapaswipasupuleti@gmail.com>
2026-05-06 23:48:51 +0530

    Update answers.sql

--- Week1/Day1/answers.sql
+++ Week1/Day1/answers.sql
@@ -1,6 +1,21 @@
-Q1) SELECT * FROM employee;
+ Q1:
+SELECT * 
+FROM employee;
 
-Q2)SELECT name FROM employee;
+Q2:
+SELECT name, salary
+FROM employee;
 
-Q3)SELECT * FROM employee
+Q3:
+SELECT *
+FROM employee
 WHERE age > 30;
+
+Q4:
+SELECT name
+FROM department;
+
+Q5:
+SELECT *
+FROM employee
+WHERE department_id = 1;
