f5dfda2b1f19e0a516d86336a51111ba74a618ad
tapaswipasupuleti <tapaswipasupuleti@gmail.com>
2026-05-06 23:47:43 +0530

    Update questions.sql

--- Week1/Day1/questions.sql
+++ Week1/Day1/questions.sql
@@ -1,3 +1,9 @@
-Q1 Select all columns from employee table
-Q2 Select employee names
-Q3 Employees older than 30
+Q1: Select all columns from the Employee table.
+
+Q2: Select only the name and salary columns from the Employee table.
+
+Q3: Select employees who are older than 30.
+
+Q4: Select the names of all departments.
+
+Q5: Select employees who work in the IT department.
